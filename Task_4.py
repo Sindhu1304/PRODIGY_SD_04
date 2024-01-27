2fb6d1481074df0537eccb22761c0682c70895f6
Sindhu1304 <138163758+Sindhu1304@users.noreply.github.com>
2024-01-28 02:16:06 +0530

    Add files via upload

---
+++ Task_4.py
@@ -0,0 +1,60 @@
+def is_valid(board, row, col, num):
+    # Check if the number is not repeated in the current row, column and 3x3 subgrid
+    for x in range(9):
+        if board[row][x] == num or board[x][col] == num:
+            return False
+
+    start_row, start_col = 3 * (row // 3), 3 * (col // 3)
+    for i in range(3):
+        for j in range(3):
+            if board[i + start_row][j + start_col] == num:
+                return False
+    return True
+
+
+def solve_sudoku(board):
+    empty = find_empty_location(board)
+    if not empty:
+        return True  # No more empty spaces, the puzzle is solved
+    row, col = empty
+
+    for num in range(1, 10):
+        if is_valid(board, row, col, num):
+            board[row][col] = num
+            if solve_sudoku(board):
+                return True
+            board[row][col] = 0  # Reset the cell for backtracking
+
+    return False  # Triggers backtracking
+
+
+def find_empty_location(board):
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] == 0:
+                return (i, j)  # row, col
+    return None
+
+
+def print_board(board):
+    for row in board:
+        print(" ".join(str(num) for num in row))
+
+
+# Example Sudoku board (0 represents empty cells)
+sudoku_board = [
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9]
+]
+
+if solve_sudoku(sudoku_board):
+    print_board(sudoku_board)
+else:
+    print("No solution exists")
